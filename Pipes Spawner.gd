055cdbe58c07a39bc11b533199eab0f298d1d857
TheRealRaichu <ilyas.mhd.khan@gmail.com>
2024-03-02 10:22:24 -0600

    test

--- Pipes Spawner.gd
+++ Pipes Spawner.gd
@@ -5,9 +5,8 @@ var pipesList = []
 
 func _on_timer_timeout():
 	if Utils.playing:
-		var rng = RandomNumberGenerator.new()
 		pipesList.append(pipe.instantiate())
-		pipesList[-1].position = Vector2(768 ,randi_range(-300, 275))
+		pipesList[-1].position = Vector2(768 , randi_range(-300, 275))
 		add_child(pipesList[-1])
 		if pipesList[0].global_position.x < -64:
 			pipesList.pop_front().queue_free()
